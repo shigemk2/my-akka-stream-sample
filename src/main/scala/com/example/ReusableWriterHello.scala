38a9017c2991b96314f30e7e3259a63e2592a5ae
shigemk2 <i.am.shige@gmail.com>
2016-04-24 21:09:13 +0900

    Add a sample writing factorial results to file

---
+++ src/main/scala/com/example/ReusableWriterHello.scala
@@ -0,0 +1,33 @@
+package com.example
+
+import java.io.File
+
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.stream.scaladsl._
+import akka.stream.{ActorMaterializer, IOResult}
+import akka.util.ByteString
+
+import scala.concurrent.Future
+
+
+object ReusableWriterHello {
+  def main(args: Array[String]): Unit = {
+    implicit val system = ActorSystem("QuickStart")
+    implicit val materializer = ActorMaterializer()
+
+    val source: Source[Int, NotUsed] = Source(1 to 100)
+    val factorials = source.scan(BigInt(1))((acc, next) => acc * next)
+
+    val result: Future[IOResult] =
+      factorials
+        .map(_.toString)
+        .runWith(lineSink("target/factorials2.txt"))
+    println("Hello, world!")
+  }
+
+  def lineSink(filename: String): Sink[String, Future[IOResult]] =
+    Flow[String]
+      .map(s => ByteString(s + "\n"))
+      .toMat(FileIO.toFile(new File(filename)))(Keep.right)
+}
