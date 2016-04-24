9c4a2b5a16f5bba2d511978801b0922203f8691b
shigemk2 <i.am.shige@gmail.com>
2016-04-24 20:56:57 +0900

    Add sample printing 1 to 100

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,7 +1,18 @@
 package com.example
 
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Source
+
+
 object Hello {
   def main(args: Array[String]): Unit = {
+    implicit val system = ActorSystem("QuickStart")
+    implicit val materializer = ActorMaterializer()
+
+    val source: Source[Int, NotUsed] = Source(1 to 100)
+    source.runForeach(i => println(i))(materializer)
     println("Hello, world!")
   }
 }
